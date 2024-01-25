15cc9d3f86f3e0f69aa7897c559a3056dbc02d43
flovelace <lovelace.af@gmail.com>
2024-01-25 12:56:16 -0500

    Added svgs to assets folder. Imported svgs to data.js, and added variables to ABOUT_DATA. Finetuned some styles for html instead of body.

--- src/data.js
+++ src/data.js
@@ -1,21 +1,30 @@
+import airImg from './assets/air.svg';
+import phosImg from './assets/phos.svg';
+import saturnImg from './assets/saturn.svg';
+import trinityImg from './assets/trinity.svg';
+
 export const ABOUT_DATA = [
   {
+    image: airImg,
     title: 'lorem',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit vitae arcu placerat auctor.',
   },
   {
+    image: phosImg,
     title: 'lorem 2',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit vitae arcu placerat auctor..',
   },
   {
+    image: saturnImg,
     title: 'lorem 3',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit vitae arcu placerat auctor.',
   },
   {
-    title: 'lorem 4',
+    image: trinityImg,
+    title: 'lorem 5',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit vitae arcu placerat auctor.',
   },
