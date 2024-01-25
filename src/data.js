da3cc5de57c6ce9f2dacfc76e2238df35c20b0fa
flovelace <lovelace.af@gmail.com>
2024-01-25 17:26:51 -0500

    Updated data.js with information for the card components.

--- src/data.js
+++ src/data.js
@@ -52,3 +52,46 @@ export const SUBGENRE_DATA = {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in velit vitae arcu placerat auctor.',
   },
 };
+
+export const CLASSICS_CARD_DATA = [
+  {
+    project: 'Secret Stairways',
+    release: 'Enchantment of the Ring',
+    year: '2017',
+  },
+  {
+    project: 'Depressive Silence',
+    release: 'Mourning',
+    year: '1996',
+  },
+  {
+    project: 'Forgotten Pathways',
+    release: 'Shrouded in Mystery',
+    year: '2017',
+  },
+  {
+    project: 'Wongraven',
+    release: 'Fjelltronen',
+    year: '1995',
+  },
+];
+
+export const NEW_PROJECTS_CARD_DATA = [
+  {
+    project: 'Hole Dweller',
+    description: 'Dungeon synth for the hobbit hearted.',
+  },
+  {
+    project: 'Gondar',
+    description: 'Dark, poignant and melancholic medieval synth.',
+  },
+  {
+    project: 'Old Sorcery',
+    description:
+      'Atmosphere and fantasical dungeon synth with a hint of black metal',
+  },
+  {
+    project: 'Diplodocus',
+    description: 'Dark and dino synth with antiquated ambience',
+  },
+];
